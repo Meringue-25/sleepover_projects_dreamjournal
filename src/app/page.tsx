554e7b81bc47c0f81cf21831806c30df4496469b
Meringue-25 <yuki.li3465@gmail.com>
2026-04-14 20:40:08 -0700

    fixed with line breaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,13 @@ const HomePage = () => {
         <h1 className="text-4xl">Welcome to Yuki's Dream Journal Website!!</h1>
         <span>
           This is for you to log your dreams. In order to log your dreams, you
+          <br />
           have to sign in with discord. At the top of the page you can find a
+          <br />
           "Sign In" button. After that you should see a "Journal" button. If you
+          <br />
           click that you will be able to start typing a title for your dream,
+          <br />
           click the create button and start typing your dream! Don't forget to
           save it!
         </span>
