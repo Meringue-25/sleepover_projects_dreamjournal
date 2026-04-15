2f40996d0ba1ecb945c0aee5681185e263e1274a
Meringue-25 <yuki.li3465@gmail.com>
2026-04-14 20:37:38 -0700

    have to commit it again to see updates on web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,13 @@ const HomePage = () => {
       <Navbar />
       <div className="mx-20 mt-10 flex flex-col text-xl">
         <h1 className="text-4xl">Welcome to Yuki's Dream Journal Website!!</h1>
-        <span>This is for you to log your dreams. In order to log your dreams, you have to sign in with discord.
-          At the top of the page you can find a "Sign In" button. After that you should see a "Journal" button.
-          If you click that you will be able to start typing a title for your dream, click the create button 
-          and start typing your dream! Don't forget to save it!
+        <span>
+          This is for you to log your dreams. In order to log your dreams, you
+          have to sign in with discord. At the top of the page you can find a
+          "Sign In" button. After that you should see a "Journal" button. If you
+          click that you will be able to start typing a title for your dream,
+          click the create button and start typing your dream! Don't forget to
+          save it!
         </span>
       </div>
     </>
